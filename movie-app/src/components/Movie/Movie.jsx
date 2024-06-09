23d1b8e594a089a76e10ee8136a9113456b13a90
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-06-09 11:06:45 +0700

    fix: style

--- movie-app/src/components/Movie/Movie.jsx
+++ movie-app/src/components/Movie/Movie.jsx
@@ -1,14 +1,16 @@
 import styles from '../Movies/Movies.module.css';
 
-export default function Movie({movie}) {
+export default function Movie({ movie }) {
     let { title, poster, year } = movie;
     return (
         <div className={styles.movie}>
-            <img
-                className={styles.movie__image}
-                src={poster}
-                alt=""
-            />
+            <div className={styles.movie__image_container}>
+                <img
+                    className={styles.movie__image}
+                    src={poster}
+                    alt=""
+                />
+            </div>
             <h3 className={styles.movie__title}>{title}</h3>
             <p className={styles.movie__date}>{year}</p>
         </div>
