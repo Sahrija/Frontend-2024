55967ba8c21f7e8db3af09587c693e411719fabd
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-06-17 15:26:52 +0700

    feat: tranlsate movie object

---
+++ movie-app/utils/movieUtils.js
@@ -0,0 +1,9 @@
+export function translateMovies(movies) {
+    return movies.map((movie) => {
+        const { title, poster_path } = movie
+        return {
+            title,
+            poster: 'https://media.themoviedb.org/t/p/w440_and_h660_face' + poster_path
+        }
+    })
+}
