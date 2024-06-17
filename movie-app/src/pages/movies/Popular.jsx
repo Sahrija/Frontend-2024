f1b5b63b4a60e4d708d61f50bbe6d351b8ad7121
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-06-17 15:41:14 +0700

    feat: consume api on popular page

--- movie-app/src/pages/movies/Popular.jsx
+++ movie-app/src/pages/movies/Popular.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
 import Movie from "../../components/Movie/Movie";
 import styles from './Popular.module.css';
+import { Link, useSearchParams } from "react-router-dom";
 
-export default function Popular({movies, setMovies}) {
-    
+import { fetchPopularMovies } from "../../services/moviesApi";
+import { translateMovies } from "../../../utils/movieUtils";
+import Button from "../../components/Button/Button";
+
+export default function Popular() {
+    const [movies, setMovies] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const page = parseInt(searchParams.get('page') || 1);
+
+    useEffect(() => {
+        fetchPopularMovies(page).then((res) => {
+            const movies = translateMovies(res);
+            setMovies(movies);
+        });
+    }, [page, setMovies, setSearchParams]);
+
     return (
         <section className={styles.container}>
             <header className=''>
                 Popular
             </header>
             <div className={styles.grid}>
-                {movies.map((movie, index) => <Movie key={index} movie={movie} />)}
+                {movies.map((movie, index) =>
+                    <Movie key={index} movie={movie} />
+                )}
             </div>
+            <Button variant='secondary'><Link className="button" to={`?page=${page - 1}`}>Prev</Link></Button>
+            <Button variant='secondary'><Link className="button" to={`?page=${page + 1}`}>Next</Link></Button>
         </section>
     )
 }
