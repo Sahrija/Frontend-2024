233a3157da9e3f5a6962f22ea2757b304c8e6547
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-06-18 17:53:07 +0700

    fix(hero): change title, genres, and overview

--- movie-app/src/components/Hero/Hero.jsx
+++ movie-app/src/components/Hero/Hero.jsx
@@ -58,12 +58,12 @@ export default function Hero() {
         <div className={styles.container}>
             <section className={styles.hero}>
                 <div className={styles.hero__left}>
-                    <h2 className={styles.hero__title}>Arcane</h2>
+                    <h2 className={styles.hero__title}>{movie.title}</h2>
                     <h3 className={styles.hero__genre}>
-                        Genre: Action, Drama, Sci-fi
+                        {genres}
                     </h3>
                     <p className={styles.hero__description}>
-                        A movie by League of Legends
+                        {movie.overview}
                     </p>
                     <Button
                         onClick={toggleTrailerModal}
