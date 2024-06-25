5f033b0cd8f33272cc75942399ab62bae230a071
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-06-26 04:43:15 +0700

    feat(api): add fetchMovie

--- movie-app/src/services/moviesApi.js
+++ movie-app/src/services/moviesApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const READ_ACCESS_TOKEN = import.meta.env.VITE_API_READ_ACCESS_TOKEN;
+const API_KEY = import.meta.env.VITE_API_KEY  ;
 const BASE_URL = 'https://api.themoviedb.org/3/movie';
  
 const config = {
@@ -27,4 +28,11 @@ export async function fetchUpcomingMovies(page = 1) {
   const URL = `${BASE_URL}/upcoming?language=en-US&page=${page}`;
   const response = await axios(URL, config);
   return response.data.results;
+}
+
+export async function fetchMovie(id) {
+  const params = `?api_key=${API_KEY}&append_to_response=videos`;
+  const url = `https://api.themoviedb.org/3/movie/${id}${params}`;
+  const response = await axios(url);
+  return response.data;
 }
